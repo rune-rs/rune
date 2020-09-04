0fa878ed6894ebf9a783156bf2f596d5bbdf3a9e
John-John Tedro <udoprog@tedro.se>
2020-09-04 15:21:34 +0200

    Release 0.6.6

--- crates/rune-modules/src/process.rs
+++ crates/rune-modules/src/process.rs
@@ -7,7 +7,7 @@
 //! Add the following to your `Cargo.toml`:
 //!
 //! ```toml
-//! rune-modules = {version = "0.6.5", features = ["process"]}
+//! rune-modules = {version = "0.6.6", features = ["process"]}
 //! ```
 //!
 //! Install it into your context:
