8934323591f59ec90ccb9e8ccfd50def448cf807
John-John Tedro <udoprog@tedro.se>
2020-09-05 11:40:38 +0200

    Release 0.6.10

--- crates/rune-modules/src/fs.rs
+++ crates/rune-modules/src/fs.rs
@@ -7,7 +7,7 @@
 //! Add the following to your `Cargo.toml`:
 //!
 //! ```toml
-//! rune-modules = {version = "0.6.9", features = ["fs"]}
+//! rune-modules = {version = "0.6.10", features = ["fs"]}
 //! ```
 //!
 //! Install it into your context:
--- crates/rune-modules/src/json.rs
+++ crates/rune-modules/src/json.rs
@@ -7,7 +7,7 @@
 //! Add the following to your `Cargo.toml`:
 //!
 //! ```toml
-//! rune-modules = {version = "0.6.9", features = ["json"]}
+//! rune-modules = {version = "0.6.10", features = ["json"]}
 //! ```
 //!
 //! Install it into your context:
