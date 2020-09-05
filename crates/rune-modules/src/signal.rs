bba1b4252eb6dc84ece8d74a6c1fc32c1ae7a412
John-John Tedro <udoprog@tedro.se>
2020-09-05 13:37:13 +0200

    Release 0.6.12

--- crates/rune-modules/src/signal.rs
+++ crates/rune-modules/src/signal.rs
@@ -7,7 +7,7 @@
 //! Add the following to your `Cargo.toml`:
 //!
 //! ```toml
-//! rune-modules = {version = "0.6.11", features = ["signal"]}
+//! rune-modules = {version = "0.6.12", features = ["signal"]}
 //! ```
 //!
 //! Install it into your context:
--- crates/rune-modules/src/toml.rs
+++ crates/rune-modules/src/toml.rs
@@ -7,7 +7,7 @@
 //! Add the following to your `Cargo.toml`:
 //!
 //! ```toml
-//! rune-modules = {version = "0.6.11", features = ["toml"]}
+//! rune-modules = {version = "0.6.12", features = ["toml"]}
 //! ```
 //!
 //! Install it into your context:
