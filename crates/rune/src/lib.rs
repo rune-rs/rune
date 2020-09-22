14592833b445782fe3ac58e13a1f0883afabf5ae
aspen <luxx4x@protonmail.com>
2020-09-22 15:15:08 -0400

    fix(modules): Fix `rand` module not compiling revert(modules): Remove `byte`/`byte_range` functions chore: Add `rand` example script

--- crates/rune/src/lib.rs
+++ crates/rune/src/lib.rs
@@ -265,6 +265,7 @@ pub fn default_context() -> Result<runestick::Context, runestick::ContextError> 
         context.install(&rune_modules::process::module()?)?;
         context.install(&rune_modules::fs::module()?)?;
         context.install(&rune_modules::signal::module()?)?;
+        context.install(&rune_modules::rand::module()?)?;
     }
 
     Ok(context)
