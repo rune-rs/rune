a0d82e76fd01db9ae1eb4dd9780987a254dc3617
github-action-benchmark <github@users.noreply.github.com>
2020-12-03 11:05:07 +0000

    add Benchmark (cargo) benchmark result for e4af457a3bb7eb0b088724703ceb56042019dec2

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1606993507795,
+  "repoUrl": "https://github.com/rune-rs/rune",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "udoprog@tedro.se",
+            "name": "John-John Tedro",
+            "username": "udoprog"
+          },
+          "committer": {
+            "email": "udoprog@tedro.se",
+            "name": "John-John Tedro",
+            "username": "udoprog"
+          },
+          "distinct": true,
+          "id": "e4af457a3bb7eb0b088724703ceb56042019dec2",
+          "message": "Add correct access token",
+          "timestamp": "2020-12-03T12:02:08+01:00",
+          "tree_id": "2969c16244064aea9052eb43615560afa812863d",
+          "url": "https://github.com/rune-rs/rune/commit/e4af457a3bb7eb0b088724703ceb56042019dec2"
+        },
+        "date": 1606993507181,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "aoc_2020_1a",
+            "value": 292557,
+            "range": "± 64930",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "aoc_2020_1b",
+            "value": 1008675,
+            "range": "± 182512",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
