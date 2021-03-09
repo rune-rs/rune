c3350f39dc92d4d3e2b8494fac25dd07787cf145
John-John Tedro <udoprog@tedro.se>
2021-03-10 00:50:29 +0100

    ssa: Rework to be less buggy

--- crates/rune-ssa/src/error.rs
+++ crates/rune-ssa/src/error.rs
@@ -1,4 +1,4 @@
-use crate::{BlockId, Var};
+use crate::{BlockId, StaticId, Var};
 use thiserror::Error;
 
 /// Error raised during machine construction.
@@ -7,8 +7,10 @@ use thiserror::Error;
 pub enum Error {
     #[error("cannot jump {0} -> {0}: destination is sealed")]
     SealedBlockJump(BlockId, BlockId),
-    #[error("missing phi node for var {0}")]
-    MissingPhiNode(Var),
+    #[error("missing phi node for static id {0}")]
+    MissingPhiNode(StaticId),
+    #[error("missing value for static id {0}")]
+    MissingValue(StaticId),
     #[error("missing block with id {0}")]
     MissingBlock(BlockId),
     #[error("tried to construct a float constant that is NaN")]
