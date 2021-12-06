da538bece2875e4785bb6fe44a6d861ffe1e36dc
John-John Tedro <udoprog@tedro.se>
2021-12-06 05:36:40 +0100

    Release 0.10.1

--- crates/rune-modules/src/signal.rs
+++ crates/rune-modules/src/signal.rs
@@ -7,7 +7,7 @@
 //! Add the following to your `Cargo.toml`:
 //!
 //! ```toml
-//! rune-modules = { version = "0.10.0", features = ["signal"] }
+//! rune-modules = { version = "0.10.1", features = ["signal"] }
 //! ```
 //!
 //! Install it into your context:
