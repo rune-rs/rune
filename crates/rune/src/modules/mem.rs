e7d193e787cbfb43e7f7e19f5b62e92f1fcb6756
John-John Tedro <udoprog@tedro.se>
2023-04-03 18:50:39 +0200

    Document ::std::mem

--- crates/rune/src/modules/mem.rs
+++ crates/rune/src/modules/mem.rs
@@ -1,16 +1,31 @@
 //! The `std::mem` module.
 
+use crate as rune;
 use crate::runtime::{Value, VmError};
 use crate::{ContextError, Module};
 
 /// Construct the `std` module.
 pub fn module() -> Result<Module, ContextError> {
     let mut module = Module::with_crate_item("std", ["mem"]);
-    module.function(["drop"], drop_impl)?;
+    module.function_meta(drop)?;
     Ok(module)
 }
 
-fn drop_impl(value: Value) -> Result<(), VmError> {
+#[rune::function]
+/// Explicitly drop the given value, freeing up any memory associated with it.
+///
+/// Normally values are dropped as they go out of scope, but with this method it
+/// can be explicitly controlled instead.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```rune
+/// let v = [1, 2, 3];
+/// drop(v);
+/// ```
+fn drop(value: Value) -> Result<(), VmError> {
     value.take()?;
     Ok(())
 }
