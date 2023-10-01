fdb55ce80ee5a7f0c8d9720013331190e1f0981e
John-John Tedro <udoprog@tedro.se>
2023-10-01 16:55:15 +0200

    Less permutations when running tests

--- tools/function_traits_permute.py
+++ tools/function_traits_permute.py
@@ -1,26 +1,43 @@
 # helper to generate function_traits/macros.rs
 
 import itertools
+import pathlib
 
-Owned = lambda var: (var, "", "FromValue", "from_value")
-Mut = lambda var: ("Mut<{}>".format(var), "&mut", "?Sized + UnsafeToMut", "unsafe_to_mut")
-Ref = lambda var: ("Ref<{}>".format(var), "&", "?Sized + UnsafeToRef", "unsafe_to_ref")
+Owned = lambda var: (var, "", ["FromValue"], "from_value")
+Mut = lambda var: (f"Mut<{var}>", "&mut", ["?Sized", "UnsafeToMut"], "unsafe_to_mut")
+Ref = lambda var: (f"Ref<{var}>", "&", ["?Sized", "UnsafeToRef"], "unsafe_to_ref")
 
-vars = ["A", "B", "C", "D", "E", "F"]
+vars = ['A', 'B', 'C', 'D', 'E']
 
-print("// Note: Automatically generated using functions_traits_permute.py")
-print("macro_rules! permute {")
-print("    ($call:path) => {")
+this = pathlib.Path(__file__)
 
-for repeat in range(0, 6):
-    for perm in itertools.product([Owned, Ref, Mut], repeat=repeat):
-        line = [str(repeat)]
+name = this.parts[-1]
+out = this.parent.parent.joinpath("crates/rune/src/module/function_traits/macros.rs")
 
-        for ((n, var), p) in zip(enumerate(vars), perm):
-            (ty, modifier, trait, coercion) = p(var)
-            line.append("{{{}, {}, {}, {}, {{{}}}, {{{}}}, {}}}".format(var, var.lower(), ty, n, modifier, trait, coercion))
+with out.open(mode = 'bw') as fd:
+    def pr(text):
+        fd.write(text.encode('utf-8'))
+        fd.write(b"\n")
 
-        print("        $call!({});".format(", ".join(line)))
+    pr(f"// Note: Automatically generated using {name}")
+    pr("macro_rules! permute {")
+    pr("    ($call:path) => {")
 
-print("    }")
-print("}")
+    for repeat in range(0, len(vars) + 1):
+        for perm in itertools.product([Owned, Ref, Mut], repeat=repeat):
+            line = [str(repeat)]
+
+            for (n, (var, p)) in enumerate(zip(vars, perm)):
+                (ty, mods, traits, coercion) = p(var)
+                var_lower = var.lower()
+                traits = " + ".join(traits)
+                line.append(f"{{{var}, {var_lower}, {ty}, {n}, {{{mods}}}, {{{traits}}}, {coercion}}}")
+
+            if repeat >= 4:
+                pr("        #[cfg(not(test))]")
+
+            args = ", ".join(line)
+            pr(f"        $call!({args});")
+
+    pr("    }")
+    pr("}")
