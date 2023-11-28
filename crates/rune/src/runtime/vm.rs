e18c6b4f7f6ea8cb411037a4f8212a5e628d30c1
John-John Tedro <udoprog@tedro.se>
2023-11-28 07:53:46 +0100

    Use same macros in vm_arithmetic

--- crates/rune/src/runtime/vm.rs
+++ crates/rune/src/runtime/vm.rs
@@ -1460,7 +1460,7 @@ impl Vm {
         protocol: Protocol,
         error: fn() -> VmErrorKind,
         integer_op: fn(i64, i64) -> Option<i64>,
-        byte_op: fn(u8, u8) -> Option<u8>,
+        byte_op: fn(u8, i64) -> Option<u8>,
     ) -> VmResult<()> {
         let lhs;
         let mut guard;
@@ -1472,7 +1472,7 @@ impl Vm {
                     *lhs = out;
                     return VmResult::Ok(());
                 }
-                (Value::Byte(lhs), Value::Byte(rhs)) => {
+                (Value::Byte(lhs), Value::Integer(rhs)) => {
                     let out = vm_try!(byte_op(*lhs, rhs).ok_or_else(error));
                     *lhs = out;
                     return VmResult::Ok(());
--- crates/rune/src/tests/vm_arithmetic.rs
+++ crates/rune/src/tests/vm_arithmetic.rs
@@ -3,61 +3,61 @@ prelude!();
 use VmErrorKind::*;
 
 macro_rules! op_tests {
-    ($lhs:literal $op:tt $rhs:literal = $out:expr) => {
-        let out: i64 = rune!(pub fn main() { let a = $lhs; let b = $rhs; a $op b});
+    ($ty:ty, $lhs:literal $op:tt $rhs:literal = $out:expr) => {
+        let out: $ty = rune!(pub fn main() { let a = $lhs; let b = $rhs; a $op b});
         assert_eq!(out, $out);
 
-        let out: i64 = rune_s!(&format!(
+        let out: $ty = rune_s!(&format!(
             r#"pub fn main() {{ let a = {lhs}; let b = {rhs}; a {op}= b; a }}"#,
-            lhs = $lhs, rhs = $rhs, op = stringify!($op),
+            lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
         ));
         assert_eq!(out, $out);
 
-        let out: i64 = rune_s!(&format!(
+        let out: $ty = rune_s!(&format!(
             r#"struct Foo {{ padding, field }}; pub fn main() {{ let a = Foo{{ padding: 0, field: {lhs} }}; let b = {rhs}; a.field {op}= b; a.field }}"#,
-            lhs = $lhs, rhs = $rhs, op = stringify!($op),
+            lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
         ));
         assert_eq!(out, $out);
 
-        let out: i64 = rune_s!(&format!(
+        let out: $ty = rune_s!(&format!(
             r#"enum Enum {{ Foo {{ padding, field }} }}; pub fn main() {{ let a = Enum::Foo {{ padding: 0, field: {lhs} }}; let b = {rhs}; a.field {op}= b; a.field }}"#,
-            lhs = $lhs, rhs = $rhs, op = stringify!($op),
+            lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
         ));
         assert_eq!(out, $out);
 
-        let out: i64 = rune_s!(&format!(
+        let out: $ty = rune_s!(&format!(
             r#"pub fn main() {{ let a = #{{ padding: 0, field: {lhs} }}; let b = {rhs}; a.field {op}= b; a.field }}"#,
-            lhs = $lhs, rhs = $rhs, op = stringify!($op),
+            lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
         ));
         assert_eq!(out, $out);
 
-        let out: i64 = rune_s!(&format!(
+        let out: $ty = rune_s!(&format!(
             r#"pub fn main() {{ let a = (0, {lhs}); let b = {rhs}; a.1 {op}= b; a.1 }}"#,
-            lhs = $lhs, rhs = $rhs, op = stringify!($op),
+            lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
         ));
         assert_eq!(out, $out);
 
-        let out: i64 = rune_s!(&format!(
+        let out: $ty = rune_s!(&format!(
             r#"struct Foo(padding, a); pub fn main() {{ let a = Foo(0, {lhs}); let b = {rhs}; a.1 {op}= b; a.1 }}"#,
-            lhs = $lhs, rhs = $rhs, op = stringify!($op),
+            lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
         ));
         assert_eq!(out, $out);
 
-        let out: i64 = rune_s!(&format!(
+        let out: $ty = rune_s!(&format!(
             r#"enum Enum {{ Foo(padding, a) }}; pub fn main() {{ let a = Enum::Foo(0, {lhs}); let b = {rhs}; a.1 {op}= b; a.1 }}"#,
-            lhs = $lhs, rhs = $rhs, op = stringify!($op),
+            lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
         ));
         assert_eq!(out, $out);
 
-        let out: i64 = rune_s!(&format!(
+        let out: $ty = rune_s!(&format!(
             r#"pub fn main() {{ let a = Ok({lhs}); let b = {rhs}; a.0 {op}= b; a.0 }}"#,
-            lhs = $lhs, rhs = $rhs, op = stringify!($op),
+            lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
         ));
         assert_eq!(out, $out);
 
-        let out: i64 = rune_s!(&format!(
+        let out: $ty = rune_s!(&format!(
             r#"pub fn main() {{ let a = Some({lhs}); let b = {rhs}; a.0 {op}= b; a.0 }}"#,
-            lhs = $lhs, rhs = $rhs, op = stringify!($op),
+            lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
         ));
         assert_eq!(out, $out);
     }
@@ -68,7 +68,7 @@ macro_rules! error_test {
         assert_vm_error!(
             &format!(
                 r#"pub fn main() {{ let a = {lhs}; let b = {rhs}; a {op} b; }}"#,
-                lhs = $lhs, rhs = $rhs, op = stringify!($op),
+                lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
             ),
             $error => {}
         );
@@ -76,7 +76,7 @@ macro_rules! error_test {
         assert_vm_error!(
             &format!(
                 r#"pub fn main() {{ let a = {lhs}; let b = {rhs}; a {op}= b; }}"#,
-                lhs = $lhs, rhs = $rhs, op = stringify!($op),
+                lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
             ),
             $error => {}
         );
@@ -84,7 +84,7 @@ macro_rules! error_test {
         assert_vm_error!(
             &format!(
                 r#"pub fn main() {{ let a = #{{ padding: 0, field: {lhs} }}; let b = {rhs}; a.field {op}= b; }}"#,
-                lhs = $lhs, rhs = $rhs, op = stringify!($op),
+                lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
             ),
             $error => {}
         );
@@ -92,7 +92,7 @@ macro_rules! error_test {
         assert_vm_error!(
             &format!(
                 r#"pub fn main() {{ let a = (0, {lhs}); let b = {rhs}; a.1 {op}= b; }}"#,
-                lhs = $lhs, rhs = $rhs, op = stringify!($op),
+                lhs = stringify!($lhs), rhs = stringify!($rhs), op = stringify!($op),
             ),
             $error => {}
         );
@@ -101,86 +101,73 @@ macro_rules! error_test {
 
 #[test]
 fn test_add() {
-    op_tests!(10 + 2 = 12);
+    op_tests!(i64, 10 + 2 = 12);
     error_test!(9223372036854775807i64 + 2 = Overflow);
 }
 
 #[test]
 fn test_sub() {
-    op_tests!(10 - 2 = 8);
+    op_tests!(i64, 10 - 2 = 8);
     error_test!(-9223372036854775808i64 - 2 = Underflow);
 }
 
 #[test]
 fn test_mul() {
-    op_tests!(10 * 2 = 20);
+    op_tests!(i64, 10 * 2 = 20);
     error_test!(9223372036854775807i64 * 2 = Overflow);
 }
 
 #[test]
 fn test_div() {
-    op_tests!(10 / 2 = 5);
+    op_tests!(i64, 10 / 2 = 5);
     error_test!(10 / 0 = DivideByZero);
 }
 
 #[test]
 fn test_rem() {
-    op_tests!(10 % 3 = 1);
+    op_tests!(i64, 10 % 3 = 1);
     error_test!(10 % 0 = DivideByZero);
 }
 
 #[test]
-fn test_byte_ops() {
-    let out: u8 = rune!(
-        pub fn main() {
-            12u8 & 6u8
-        }
-    );
-    assert_eq!(out, 4u8);
-
-    let out: u8 = rune!(
-        pub fn main() {
-            12u8 ^ 6u8
-        }
-    );
-    assert_eq!(out, 10u8);
-
-    let out: u8 = rune!(
-        pub fn main() {
-            12u8 | 6u8
-        }
-    );
-    assert_eq!(out, 14u8);
-
-    let out: u8 = rune!(
-        pub fn main() {
-            12u8 << 2
-        }
-    );
-    assert_eq!(out, 48u8);
-
-    let out: u8 = rune!(
-        pub fn main() {
-            12u8 >> 2
-        }
-    );
-    assert_eq!(out, 3u8);
-}
-
-#[test]
-fn test_bit_ops() {
-    op_tests!(0b1100 & 0b0110 = 0b1100 & 0b0110);
-    op_tests!(0b1100 ^ 0b0110 = 0b1100 ^ 0b0110);
-    op_tests!(0b1100 | 0b0110 = 0b1100 | 0b0110);
-    op_tests!(0b1100 << 2 = 0b1100 << 2);
-    op_tests!(0b1100 >> 2 = 0b1100 >> 2);
+fn test_bit_ops_i64() {
+    op_tests!(i64, 0b1100 & 0b0110 = 0b1100 & 0b0110);
+    op_tests!(i64, 0b1100 ^ 0b0110 = 0b1100 ^ 0b0110);
+    op_tests!(i64, 0b1100 | 0b0110 = 0b1100 | 0b0110);
+    op_tests!(i64, 0b1100 << 2 = 0b1100 << 2);
+    op_tests!(i64, 0b1100 >> 2 = 0b1100 >> 2);
     error_test!(0b1 << 64 = Overflow);
 }
 
 #[test]
-fn test_bitwise_not() {
-    let out: i64 = rune! {
-        pub fn main() { let a = 0b10100; !a }
-    };
+fn test_bit_ops_u8() {
+    op_tests!(u8, 0b1100u8 & 0b0110u8 = 0b1100u8 & 0b0110u8);
+    op_tests!(u8, 0b1100u8 ^ 0b0110u8 = 0b1100u8 ^ 0b0110u8);
+    op_tests!(u8, 0b1100u8 | 0b0110u8 = 0b1100u8 | 0b0110u8);
+    op_tests!(u8, 0b1100u8 << 2 = 0b1100u8 << 2);
+    op_tests!(u8, 0b1100u8 >> 2 = 0b1100u8 >> 2);
+    error_test!(0b1u8 << 8 = Overflow);
+    error_test!(0b1u8 >> 8 = Underflow);
+}
+
+#[test]
+fn test_bitwise_not_i64() {
+    let out: i64 = rune!(
+        pub fn main() {
+            let a = 0b10100;
+            !a
+        }
+    );
     assert_eq!(out, !0b10100);
+}
+
+#[test]
+fn test_bitwise_not_u8() {
+    let out: u8 = rune!(
+        pub fn main() {
+            let a = 0b10100u8;
+            !a
+        }
+    );
+    assert_eq!(out, !0b10100u8);
 }
