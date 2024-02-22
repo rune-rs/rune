rune-rs/rune

commit 79e81a02d01ac8d40fa8c7b595bd7262282dd552

Create a public function for source formatting (#665)
* Create a public function for source code formatting.
* Add `use crate::alloc::String;`
* Make fmt module public

crates/rune/src/fmt.rs
@@ -9,6 +9,7 @@ mod indent_writer;
 mod printer;
 mod whitespace;
 
+use crate::alloc::String;
 use crate::alloc::Vec;
 use crate::ast;
 use crate::parse::{Parse, Parser};
@@ -25,4 +26,10 @@ pub(crate) fn layout_source(source: &str) -> Result<Vec<u8>, FormattingError> {
     let mut printer: Printer = Printer::new(source)?;
     printer.visit_file(&ast)?;
     printer.commit()
+}
+
+/// Format the given source.
+pub fn format_source(source: &str) -> Result<String, impl std::error::Error> {
+    let formatted = layout_source(source)?;
+    Ok(String::from_utf8(formatted).unwrap())
 }

crates/rune/src/lib.rs
@@ -197,7 +197,7 @@ mod exported_macros;
 pub mod ast;
 
 #[cfg(feature = "fmt")]
-pub(crate) mod fmt;
+pub mod fmt;
 
 cfg_emit! {
     pub use ::codespan_reporting::term::termcolor;

commit 81a6e53a71e9478d43b990a08968493a241ab130

Fix build error caused by #665

crates/rune/src/fmt.rs
@@ -31,5 +31,5 @@ pub(crate) fn layout_source(source: &str) -> Result<Vec<u8>, FormattingError> {
 /// Format the given source.
 pub fn format_source(source: &str) -> Result<String, impl std::error::Error> {
     let formatted = layout_source(source)?;
-    Ok(String::from_utf8(formatted).unwrap())
+    Ok::<String, FormattingError>(String::from_utf8(formatted).unwrap())
 }