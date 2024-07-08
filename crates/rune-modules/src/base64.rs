5ffa0f4784446b32c03521a72a02191957495333
Robert Schütte <schuetterobert@gmail.com>
2024-07-08 21:17:56 +0200

    Added error type for base64

--- crates/rune-modules/src/base64.rs
+++ crates/rune-modules/src/base64.rs
@@ -1,5 +1,9 @@
 use base64::prelude::*;
-use rune::{ContextError, Module};
+use rune::alloc::fmt::TryWrite;
+use rune::{
+    runtime::{Formatter, VmResult},
+    ContextError, Module,
+};
 
 /// Construct the `base64` module.
 pub fn module(_stdio: bool) -> Result<Module, ContextError> {
@@ -10,6 +14,8 @@ pub fn module(_stdio: bool) -> Result<Module, ContextError> {
         "This module provides functions to encode & decode base64 data.",
     ])?;
 
+    module.ty::<Error>()?;
+
     module.function_meta(decode)?;
     module.function_meta(encode)?;
     Ok(module)
@@ -18,12 +24,11 @@ pub fn module(_stdio: bool) -> Result<Module, ContextError> {
 /// Decode a base64 String into data
 ///
 /// ```rune
-/// //assert_eq!(base64::decode("+uwgVQA=")?, b"\xFA\xEC\x20\x55\0");
-/// assert!(base64::decode("+uwgVQA=").is_ok());
+/// assert_eq!(base64::decode("+uwgVQA=")?, b"\xFA\xEC\x20\x55\0");
 /// ```
 #[rune::function]
-fn decode(inp: &str) -> Result<Vec<u8>, String> {
-    BASE64_STANDARD.decode(inp).map_err(|e| e.to_string())
+fn decode(inp: &str) -> Result<Vec<u8>, Error> {
+    Ok(BASE64_STANDARD.decode(inp)?)
 }
 
 /// Encode a data into a base64 String.
@@ -34,4 +39,26 @@ fn decode(inp: &str) -> Result<Vec<u8>, String> {
 #[rune::function]
 fn encode(bytes: &[u8]) -> String {
     BASE64_STANDARD.encode(bytes)
+}
+
+/// An error returned by methods in the `base64` module.
+#[derive(Debug, rune::Any)]
+#[rune(item = ::base64)]
+#[allow(dead_code)]
+pub struct Error {
+    inner: base64::DecodeError,
+}
+
+impl From<base64::DecodeError> for Error {
+    fn from(inner: base64::DecodeError) -> Self {
+        Self { inner }
+    }
+}
+
+impl Error {
+    #[rune::function(instance, protocol = STRING_DISPLAY)]
+    fn string_display(&self, f: &mut Formatter) -> VmResult<()> {
+        rune::vm_write!(f, "{}", self.inner);
+        VmResult::Ok(())
+    }
 }
