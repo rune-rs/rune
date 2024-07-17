952b5fb726377f5afbf61fd415f20e92b8687c85
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2024-07-17 10:54:51 +0200

    rune: Backport changes to Value::string_debug_with
    This ensures that we don't get an error on formatting a value without a debug protocol.
    
    (fixes #747)

--- crates/rune/src/runtime/value.rs
+++ crates/rune/src/runtime/value.rs
@@ -495,11 +495,16 @@ impl Value {
                 vm_write!(f, "{:?}", value);
             }
             value => {
+                // reborrow f to avoid moving it
                 let result =
-                    vm_try!(caller.call_protocol_fn(Protocol::STRING_DEBUG, value.clone(), (f,),));
+                    caller.call_protocol_fn(Protocol::STRING_DEBUG, self.clone(), (&mut *f,));
 
-                vm_try!(<()>::from_value(result));
-                return VmResult::Ok(());
+                if let VmResult::Ok(result) = result {
+                    vm_try!(<()>::from_value(result));
+                } else {
+                    let type_info = vm_try!(value.type_info());
+                    vm_write!(f, "<{} object at {:p}>", type_info, value);
+                }
             }
         };
 
