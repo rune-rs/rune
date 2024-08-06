76783dedd41d7e00233f1f6e4fad02013a65d8cc
John-John Tedro <udoprog@tedro.se>
2024-08-06 13:30:01 +0200

    base64: fix nits

--- crates/rune-modules/src/base64.rs
+++ crates/rune-modules/src/base64.rs
@@ -3,18 +3,24 @@ use rune::alloc::fmt::TryWrite;
 use rune::alloc::{String, Vec};
 use rune::runtime::Bytes;
 use rune::{
-    runtime::{Formatter, VmResult},
     ContextError, Module,
 };
+use rune::runtime::{Formatter, VmResult};
 
-/// Construct the `base64` module.
+#[rune::module(::base64)]
+/// Correct and fast [base64] encoding based on the [`base64`] crate.
+///
+/// [base64]: https://developer.mozilla.org/en-US/docs/Glossary/Base64
+/// [`base64`]: https://docs.rs/base64
+///
+/// # Examples
+///
+/// ```rune
+/// let encoded = base64::encode(b"\xFF\xEC\x20\x55\0");
+/// assert_eq!(base64::decode(encoded), Ok(b"\xFF\xEC\x20\x55\0"));
+/// ```
 pub fn module(_stdio: bool) -> Result<Module, ContextError> {
-    let mut module = Module::with_crate("base64")?;
-    module.item_mut().docs([
-        "Base64 encoding and decoding",
-        "",
-        "This module provides functions to encode & decode base64 data.",
-    ])?;
+    let mut module = Module::from_meta(self::module_meta)?;
 
     module.ty::<DecodeError>()?;
 
@@ -25,32 +31,29 @@ pub fn module(_stdio: bool) -> Result<Module, ContextError> {
 
 /// Decode a base64 String into data
 ///
+/// # Examples
+///
 /// ```rune
 /// assert_eq!(base64::decode("+uwgVQA=")?, b"\xFA\xEC\x20\x55\0");
 /// ```
 #[rune::function(vm_result)]
 fn decode(inp: &str) -> Result<Bytes, DecodeError> {
     // estimate the max size
-    let mut decoded_size = base64::decoded_len_estimate(inp.len());
+    let decoded_size = base64::decoded_len_estimate(inp.len());
 
     // try to allocate enough bytes
-    let mut v: Vec<u8> = Vec::new();
-    v.try_resize_with(decoded_size, || 0).vm?;
-    let mut bytes = Bytes::from_vec(v);
+    let mut v = Vec::new();
+    v.try_resize(decoded_size, 0).vm?;
 
     // decode
-    let len = BASE64_STANDARD.decode_slice(inp, &mut bytes)?;
-
-    // remove bytes which where to much
-    while decoded_size > len {
-        bytes.pop();
-        decoded_size -= 1;
-    }
-
-    Ok(bytes)
+    let len = BASE64_STANDARD.decode_slice(inp, &mut v)?;
+    v.truncate(len);
+    Ok(Bytes::from_vec(v))
 }
 
 /// Encode a data into a base64 String.
+///
+/// # Examples
 ///
 /// ```rune
 /// assert_eq!(base64::encode(b"\xFF\xEC\x20\x55\0"), "/+wgVQA=");
@@ -58,31 +61,31 @@ fn decode(inp: &str) -> Result<Bytes, DecodeError> {
 #[rune::function(vm_result)]
 fn encode(bytes: &[u8]) -> String {
     let Some(encoded_size) = base64::encoded_len(bytes.len(), true) else {
-        return VmResult::panic("Input bytes overflow usize");
+        VmResult::panic("encoded input length overflows usize").vm?
     };
 
-    let mut output_buf: Vec<u8> = Vec::new();
-    output_buf
-        .try_resize_with(encoded_size, Default::default)
+    let mut buf = Vec::new();
+    buf
+        .try_resize(encoded_size, 0)
         .vm?;
 
     // this should never panic
-    if BASE64_STANDARD
-        .encode_slice(bytes, &mut output_buf)
-        .is_err()
+    if let Err(e) = BASE64_STANDARD
+        .encode_slice(bytes, &mut buf)
     {
-        VmResult::panic("Can't encode to base64 String").vm?;
+        VmResult::panic(e).vm?;
     }
 
     // base64 should only return valid utf8 strings
-    let Ok(string) = String::from_utf8(output_buf) else {
-        return VmResult::panic("Can't crate valid utf8 string");
+    let string = match String::from_utf8(buf) {
+        Ok(s) => s,
+        Err(e) => VmResult::panic(e).vm?,
     };
 
     string
 }
 
-/// An error returned by methods in the `base64` module.
+/// Errors that can occur while decoding.
 #[derive(Debug, rune::Any)]
 #[rune(item = ::base64)]
 #[allow(dead_code)]
