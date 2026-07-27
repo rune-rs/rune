75c9c65376d82e4f0a5e2f29e17fdf37dfa64a16
John-John Tedro <udoprog@tedro.se>
2026-07-27 15:39:07 +0200

    Bump version to 0.15.0

--- crates/rune-modules/src/json.rs
+++ crates/rune-modules/src/json.rs
@@ -7,7 +7,7 @@
 //! Add the following to your `Cargo.toml`:
 //!
 //! ```toml
-//! rune-modules = { version = "0.14.0", features = ["json"] }
+//! rune-modules = { version = "0.15.0", features = ["json"] }
 //! ```
 //!
 //! Install it into your context:
--- crates/rune-modules/src/rand/mod.rs
+++ crates/rune-modules/src/rand/mod.rs
@@ -8,7 +8,7 @@
 //! Add the following to your `Cargo.toml`:
 //!
 //! ```toml
-//! rune-modules = { version = "0.14.0", features = ["rand"] }
+//! rune-modules = { version = "0.15.0", features = ["rand"] }
 //! ```
 //!
 //! Install it into your context:
